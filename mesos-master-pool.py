ee81f607bbc6a4fe852ad341c98dde111d71a4e4
System <Mr.NN91@gmail.com>
2016-11-23 22:09:25 +0700

    Create mesos-master-pool.py

---
+++ mesos-master-pool.py
@@ -0,0 +1,40 @@
+
+
+# http://www.thedevpiece.com/building-a-microservice-infrastructure-using-mesos-marathon-and-zookeeper/
+
+
+# Building a Microservice Infrastructure using Mesos, Marathon and Zookeeper
+
+
+# master pool, slave pool, proxy pool
+
+
+# Installing the repository
+rpm -Uvh http://repos.mesosphere.com/el/7/noarch/RPMS/mesosphere-el-repo-7-1.noarch.rpm
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
