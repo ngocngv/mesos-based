4f874f80593e602229470a5dc1f7ea5f1516c990
System <Mr.NN91@gmail.com>
2016-11-23 22:16:32 +0700

    Update mesos-master-pool.py

--- mesos-master-pool.py
+++ mesos-master-pool.py
@@ -13,12 +13,20 @@
 rpm -Uvh http://repos.mesosphere.com/el/7/noarch/RPMS/mesosphere-el-repo-7-1.noarch.rpm
 
 
+# Installing Zookeeper, Marathon and Mesos in the master pool
+yum -y install mesos marathon mesosphere-zookeeper
 
 
+# Configuring Zookeeper
+# Set /etc/zookeeper/conf/myid to the id of the current master.
+echo 1 > /etc/zookeeper/conf/myid
 
-
-
-
+# Configure /etc/zookeeper/conf/zoo.cfg, informing each machine our cluster will have.
+server.1=master-1:2888:3888  
+server.2=master-2:2888:3888  
+server.3=master-3:2888:3888  
+server.4=master-4:2888:3888  
+server.5=master-5:2888:3888
 
 
 
