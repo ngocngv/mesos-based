db82b3fd5e2b1a60b50f2d19373c2999a18364b4
System <Mr.NN91@gmail.com>
2016-11-23 22:19:28 +0700

    Create mesos.py

---
+++ mesos.py
@@ -0,0 +1,21 @@
+#Number	Pool	    Hostname
+
+#1	    Slave	    slave-1
+#2	    Slave	    slave-2
+#3	    Slave	    slave-3
+#4	    Slave	    slave-4
+#5	    Slave	    slave-5
+#6	    Slave	    slave-6
+#7	    Slave	    slave-7
+#8	    Slave	    slave-8
+#9	    Slave	    slave-9
+
+#10	    Master	  master-1
+#11	    Master	  master-2
+#12	    Master	  master-3
+#13	    Master	  master-4
+#14	    Master	  master-5
+
+#15	    Proxy	    proxy-1
+#16	    Proxy	    proxy-2
+#17	    Proxy	    proxy-3
