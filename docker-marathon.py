1b7eba2d4de44d769faa6affb90452bdd5e7403f
System <Mr.NN91@gmail.com>
2016-11-25 10:36:47 +0700

    Update docker-marathon.py

--- docker-marathon.py
+++ docker-marathon.py
@@ -1,5 +1,8 @@
 
 # http://www.thedevpiece.com/deploying-and-running-docker-containers-on-marathon/
+# https://mesosphere.github.io/marathon/docs/native-docker.html
+
+
 
 # Configuring Mesos slaves to run Docker containers
 
@@ -18,8 +21,12 @@ systemctl start docker
 # configure slave machines to also be able to run docker containers:
 echo 'docker,mesos' > /etc/mesos-slave/containerizers
 
-# And increase the executor timeout for a possible delay in downloading an image:
-echo '5mins' > /etc/mesos-slave/executor_registration_timeout
+# Increase the executor timeout to account for the potential delay pulling a docker image to the agent node.
+# echo '5mins' > /etc/mesos-slave/executor_registration_timeout
+echo '10mins' > /etc/mesos-slave/executor_registration_timeout
+
+# Restart the agent process to load the new configuration.
+
 
 
 
