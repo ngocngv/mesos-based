42497b659b9ea163ddc536b2b0bd69f4a2849043
System <Mr.NN91@gmail.com>
2016-11-27 13:54:06 +0700

    Update deploy-mesos-slave.py

--- deploy-mesos-slave.py
+++ deploy-mesos-slave.py
@@ -1,1 +1,38 @@
 
+
+
+
+# configure slave machines to also be able to run docker containers:
+echo 'docker,mesos' > /etc/mesos-slave/containerizers
+
+# Increase the executor timeout to account for the potential delay pulling a docker image to the agent node.
+# echo '5mins' > /etc/mesos-slave/executor_registration_timeout
+echo '10mins' > /etc/mesos-slave/executor_registration_timeout
+
+# Restart the agent process to load the new configuration.
+
+
+
+
+
+
+# systemctl enable mesos-slave
+# systemctl start mesos-slave
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
