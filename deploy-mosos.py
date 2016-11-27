499766fa0b4e25e7b87e9b812c20dc8bf9e3e165
System <Mr.NN91@gmail.com>
2016-11-27 13:03:50 +0700

    Create deploy-mosos.py

---
+++ deploy-mosos.py
@@ -0,0 +1,32 @@
+
+
+# Installing the repository
+# rpm -Uvh http://repos.mesosphere.com/el/7/noarch/RPMS/mesosphere-el-repo-7-1.noarch.rpm
+rpm -Uvh http://repos.mesosphere.com/el/7/noarch/RPMS/mesosphere-el-repo-7-3.noarch.rpm
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
