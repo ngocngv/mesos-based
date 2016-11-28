33319f2e2ce02f8c66ddfb2b24f9d9bc899ebb9d
System <Mr.NN91@gmail.com>
2016-11-29 00:26:29 +0700

    Update deploy-calico.py

--- deploy-calico.py
+++ deploy-calico.py
@@ -16,19 +16,19 @@
 
 
 # Make dirs if they don't exist
-mkdir -p /data/mesos/cni/plugins
-mkdir -p /data/mesos/cni/conf.d
+mkdir -p /var/lib/mesos/cni/plugins
+mkdir -p /var/lib/mesos/cni/conf.d
 
 # Add location of binary and conf directories for CNI.
-echo '/data/mesos/cni/plugins' | tee /etc/mesos-slave/network_cni_plugins_dir
-echo '/data/mesos/cni/conf.d' | tee /etc/mesos-slave/network_cni_config_dir
+echo '/var/lib/mesos/cni/plugins' | tee /etc/mesos-slave/network_cni_plugins_dir
+echo '/var/lib/mesos/cni/conf.d' | tee /etc/mesos-slave/network_cni_config_dir
 
 
 # find /etc/mesos-slave/* | xargs cat
 
 
 #
-cd /data/mesos/cni/plugins
+cd /var/lib/mesos/cni/plugins
 wget https://github.com/containernetworking/cni/releases/download/v0.3.0/cni-v0.3.0.tgz
 tar xvf cni-*.tgz
 
@@ -45,7 +45,7 @@ tar xvf cni-*.tgz
 # cd cni
 # git checkout v0.3.0
 # ./build
-# cp bin/* /data/mesos/cni/plugins
+# cp bin/* /var/lib/mesos/cni/plugins
 
 
 
@@ -54,7 +54,7 @@ tar xvf cni-*.tgz
 #--------------
 
 # Add example Mesos CNI plugin configuration
-cat << '__EOF__' | tee /data/mesos/cni/conf.d/10-bridge.conf
+cat << '__EOF__' | tee /var/lib/mesos/cni/conf.d/10-bridge.conf
 {
     "cniVersion": "0.3.0",
     "name": "bridge-10-200",
@@ -74,7 +74,7 @@ __EOF__
 
 
 #
-cat << '__EOF__' | tee /data/mesos/cni/conf.d/99-loopback.conf
+cat << '__EOF__' | tee /var/lib/mesos/cni/conf.d/99-loopback.conf
 {
     "cniVersion": "0.3.0",
     "type": "loopback"
@@ -89,7 +89,43 @@ __EOF__
 
 
 
-http://severalnines.com/blog/mysql-docker-multi-host-networking-mysql-containers-part-2-calico
+
+
+
+# CoreOS Etcd – A highly available key-value store for shared configuration and service discovery.
+# CoreOS Etcd is used by Calico to store network configurations.
+
+
+# create directory /var/lib/etcd, /etc/etcd, add the etcd user and group:
+
+mkdir /var/lib/etcd
+mkdir /etc/etcd 
+
+groupadd -r etcd
+useradd -r -g etcd -d /var/lib/etcd -s /sbin/nologin -c "etcd user" etcd
+
+chown -R etcd:etcd /var/lib/etcd
+
+
+#
+https://github.com/coreos/etcd/releases/download/v3.0.15/etcd-v3.0.15-linux-amd64.tar.gz
+tar xvf etcd-*.tar.gz
+
+cp etcd /usr/local/bin/
+cp etcdctl /usr/local/bin/
+
+
+
+
+
+
+
+
+# https://docs.onegini.com/cim/idp/2.39.01-SNAPSHOT/installation/etcd.html
+# https://n40lab.wordpress.com/2016/08/01/installing-coreos-etcd-server-on-centos-7/
+# http://severalnines.com/blog/mysql-docker-multi-host-networking-mysql-containers-part-2-calico
+
+
 
 
 
