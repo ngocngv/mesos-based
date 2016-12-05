a59a0a139e40954f4f9e5b89b9f53881e5142bd4
System <Mr.NN91@gmail.com>
2016-12-05 14:20:30 +0700

    Update usage-calico-policy-libnetwork.py

--- usage-calico-policy-libnetwork.py
+++ usage-calico-policy-libnetwork.py
@@ -1,1 +1,70 @@
 
+
+# Accessing Calico policy with Calico as a network plugin
+# http://docs.projectcalico.org/v2.0/getting-started/docker/tutorials/advanced-policy
+
+
+
+
+docker network create --driver calico --ipam-driver calico-ipam database 
+docker network create --driver calico --ipam-driver calico-ipam frontend 
+
+
+
+# Create the profiles
+# Profile Resource (profile)
+# http://docs.projectcalico.org/v2.0/reference/calicoctl/resources/profile
+
+# Use calicoctl apply to create or update the profiles:
+#------------------------------------------------------------------------------
+cat << '__EOF__' | /usr/local/bin/calicoctl apply -f -
+- apiVersion: v1
+  kind: profile
+  metadata:
+    name: database
+    labels:
+      role: database
+  spec:
+    ingress:
+    - action: allow
+      protocol: tcp
+      source:
+        selector: role == 'frontend'
+      destination:
+        ports:
+        -  3306
+    - action: allow
+      source:
+        selector: role == 'database'
+    egress:
+    - action: allow
+      destination:
+        selector: role == 'database'
+- apiVersion: v1
+  kind: profile
+  metadata:
+    name: frontend
+    labels:
+      role: frontend
+  spec:
+    egress:
+    - action: allow
+      protocol: tcp
+      destination:
+        selector: role == 'database'
+        ports:
+        -  3306
+__EOF__
+
+
+
+
+
+
+
+
+
+
+
+
+
