86e0c3a52ea5cce011b3bbb3084365b6c4ede647
System <Mr.NN91@gmail.com>
2016-12-06 12:01:32 +0700

    Update deploy-mesos-openvpn.py

--- deploy-mesos-openvpn.py
+++ deploy-mesos-openvpn.py
@@ -1,1 +1,70 @@
 
+
+# http://blog.bdgn.net/a-reference-mesos-cluster-setup-3
+
+
+
+
+# Marathon config for OpenVPN
+#-----------------------------------------------------------------------
+
+cat << '__EOF__' | tee openvpn-marathon.json
+{
+  "id": "/openvpn",
+  "container": {
+    "type": "DOCKER",
+    "docker": {
+      "image": "docker-registry.marathon.mesos:5000/openvpn",
+      "network": "USER",
+      "portMappings": [
+        { "hostPort": 0, 
+          "containerPort": 8080
+        }
+      ],
+      "forcePullImage":true
+    }
+  },  
+  "instances": 1,
+  "cpus": 0.1,
+  "mem": 128,
+  "ipAddress": {
+      "networkName": "management-ui"
+  },
+
+  "dependencies": ["/mesos-dns", "/docker-registry"],
+  "healthChecks": [{"protocol": "TCP"}]
+}
+__EOF__
+
+
+
+# Starting the OpenVPN service:
+curl -sL -X POST \
+  -H 'content-type: application/json' \
+  leader.mesos:8080/v2/apps \
+  -d@openvpn-marathon.json | jq .
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
