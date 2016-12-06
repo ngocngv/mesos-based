a97ade3279592c4e0ae33d994a2afb808d4ea3ba
System <Mr.NN91@gmail.com>
2016-12-06 17:06:16 +0700

    Update deploy-openvpn-client.py

--- openvpn/deploy-openvpn-client.py
+++ openvpn/deploy-openvpn-client.py
@@ -1,1 +1,37 @@
 
+
+# https://www.unixmen.com/install-openvpn-centos-7/
+
+
+#--------------------------------------------------------------------------------------
+# Step 6 — Configuring a Client
+#--------------------------------------------------------------------------------------
+
+# copy of the ca certificate from the server, along with the client key and certificate.
+/etc/openvpn/easy-rsa/keys/ca.crt
+/etc/openvpn/easy-rsa/keys/client.crt
+/etc/openvpn/easy-rsa/keys/client.key
+
+
+
+# openvon.ovpn
+#--------------------------------------------------------------------
+client
+dev tun
+proto udp
+remote your_server_ip 1194
+resolv-retry infinite
+nobind
+persist-key
+persist-tun
+comp-lzo
+verb 3
+ca /path/to/ca.crt
+cert /path/to/client.crt
+key /path/to/client.key
+
+
+
+
+
+
